add2abedab557ce7482e01a9165ca726344f5e8c
Thomas van Dongen <thomas123@live.nl>
2018-12-09 13:47:13 +0100

    Wrote KNN function for assignment 2 and 3

--- lab-3/assignment-2/knn_wrapper.m
+++ lab-3/assignment-2/knn_wrapper.m
@@ -9,6 +9,19 @@ nr_of_classes = 2;
 % Class labels
 class_labels = floor( (0:length(data)-1) * nr_of_classes / length(data) );
 
+
+function class = KNN(dims, K, data, class_labels)
+  distances = zeros(size(data));
+  for i=1:length(data)
+    points = [dims;data(i,:)];
+    distance = pdist(points, 'euclidean');
+    distances(i) = distance;
+  end;
+  [distances_sorted, distances_order] = sort(distances);
+  class_sorted = class_labels(distances_order);
+  class = mode(class_sorted(1:K));
+end;
+
 % Sample the parameter space
 result=zeros(samples);
 for i=1:samples
