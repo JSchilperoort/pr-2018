bb906c3b999be48165aed6233fa908ba92ab8d6a
Pringled <thomas123@live.nl>
2018-12-17 14:09:41 +0100

    Finished assignment 1

--- lab-4/assignment-1/lab4_1.m
+++ lab-4/assignment-1/lab4_1.m
@@ -3,8 +3,9 @@ c = imread('Cameraman.tif');
 
 edges = edge(c, 'canny');
 
-%hc = hough(edges);
-[hc,theta,rho]	=	hough(edges);	
+[hc,theta,rho]	=	hough(edges);
+
+% Code for question 3
 %{
 imagesc(hc);
 xlabel('\theta(degrees)');
@@ -14,49 +15,40 @@ title('s2984318');
 
 hcTh = hc;
 hcTh(hcTh < 0.999 * max(hc(:))) = 0;
-%imagesc(hcTh);
 
-P	=	houghpeaks(hc,5);	
-x	=	(P(:,2));	
-y	=	(P(:,1));	
+P	=	houghpeaks(hc,5);
+x	=	theta(P(:,2));	
+y	=	rho(P(:,1));	
 
-
+% Code for question 5
 %{
-imagesc(hc);
-hold on;
-
+	figure	
+		imshow(imadjust(mat2gray(hc)),[],...	
+				'XData',theta,...	
+				'YData',rho,...	
+				'InitialMagnification','fit');	
+		xlabel('\theta	(degrees)')	
+		ylabel('\rho')	
+		axis	on	
+		axis	normal	
+		hold	on	
+		colormap(hot)	
+        
 plot(x,y,'s','color','black');	
 xlabel('\theta(degrees)');
 ylabel('\rho');
 title('s2984318');
 %}
 
-%imagesc(c);
+% Code for question 7
 imshow(c);
-hold on;
-%myhoughline(c, y, x);
-
+title('s2984318');
 
 for i=1:1
-    x = (P(i,1));
-    y = (P(i,2));
-
-    myhoughline(edges, y, x);
+    y = rho(P(i,1));
+    x = theta(P(i,2));
+    myhoughline(c, y, x);
 end
-
-
-
-%lines	=	houghlines(edges,theta,rho,P,'FillGap',5,'MinLength',7);
-
-%{
-lines	=	houghlines(edges,theta,rho,P);
-max_len	=	0;	
-		for	k	=	1:length(lines)	
-					xy	=	[lines(k).point1;	lines(k).point2];	
-					plot(xy(:,1),xy(:,2),'LineWidth', 2,'Color','green');	
-		end	
-%}
-
 
 function myhoughline(image,r,theta)
 
@@ -71,17 +63,5 @@ else
 end    
 end
 
-%{
-function myhoughline(edges,r,theta)
 
-[x,y]=size(image);
-angle=deg2rad(theta);
 
-if sin(angle)==0
-    line([r r],[0,y],'Color','red')
-  
-else
-    line([0,y],[r/sin(angle),(r-y*cos(angle))/sin(angle)],'Color','red')
-end    
-end
-%}
