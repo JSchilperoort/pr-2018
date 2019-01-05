6156426e7cf22f4d912b201f5b03710a79a77266
Jits Schilperoort <jits@RN-145-90-171-53.eduroam.rug.nl>
2019-01-05 16:52:46 +0100

    finished assignment 1 until 5-5 update

--- lab-5/assignment-1/lab_5_1_code.m
+++ lab-5/assignment-1/lab_5_1_code.m
@@ -40,10 +40,10 @@ function f = lab5_1()
     %Plot the error over time
     plot(1:epochs, Error ./ P .* 100);
     ylim([0 100]);
-    xlabel("Time")
-    ylabel("Misclassificaion error in %")
-    title("Error over time")
-    saveas(fig2,"lab5_4.png")
+    xlabel("Time");
+    ylabel("Misclassificaion error in %");
+    title("Error over time");
+    saveas(fig2,"lab5_4.png");
     
     hold off
     fig3 = figure(3);
@@ -58,13 +58,10 @@ function f = lab5_1()
         
     end
     legend('A=1, B=1','A=1, B=2','A=2, B=1','A=2, B=2');
-    xlabel("Time")
-    ylabel("Misclassificaion error in %")
-    title("Error over time")
-    saveas(fig3,"lab5_5.png")
-
-
-    
+    xlabel("Time");
+    ylabel("Misclassificaion error in %");
+    title("Error over time");
+    saveas(fig3,"lab5_5.png");
     
 end
 
